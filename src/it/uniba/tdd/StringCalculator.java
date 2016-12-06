5a57d1b5d7c78bf616c035fbeaa8cce9d425c0ec
MengaPierpaolo <pierpaolo.menga@gmail.com>
2016-12-06 15:57:04 +0100

    Tasks done

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,43 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+	public int add(String numbersStr) throws StringCalculatorException {
+		if (numbersStr == "")
+			return 0;
+		int decimal = 0;
+		for (int i = 0; i < numbersStr.length(); i++){
+			if((numbersStr.charAt(i)==',' && numbersStr.charAt(i+1)=='\n')||(numbersStr.charAt(i)=='\n' && numbersStr.charAt(i+1)==',')) 
+				throw new StringCalculatorException();
+			if(numbersStr.charAt(i)=='-' && numbersStr.charAt(i+1) >= '0' && numbersStr.charAt(i+1) <= '9')
+				throw new StringCalculatorException();
+		}
+		String[] parts = numbersStr.split("[,\n]");
+		for (int i = 0; i < parts.length; i++)
+			decimal += Integer.parseInt(parts[i]);
+		return decimal;
+	}
+	
+	public int addWithCustomDelimiter(String numbersStr) throws StringCalculatorException {
+		if (numbersStr == "")
+			return 0;
+		int decimal = 0;
+		for (int i = 0; i < numbersStr.length(); i++){
+			if((numbersStr.charAt(i)==',' && numbersStr.charAt(i+1)=='\n')||(numbersStr.charAt(i)=='\n' && numbersStr.charAt(i+1)==',')) 
+				throw new StringCalculatorException();
+			if(numbersStr.charAt(i)=='-' && numbersStr.charAt(i+1) >= '0' && numbersStr.charAt(i+1) <= '9')
+				throw new StringCalculatorException();
+		}
+		String splitter=numbersStr.split("\n")[0].substring(2);
+		for (int i = 0; i < splitter.length(); i++){
+			System.out.println(splitter.charAt(i));
+		}
+		String[] parts = numbersStr.split("\n")[1].split(splitter);
+		for (int i = 0; i < parts.length; i++)
+			System.out.println(parts[i]);
+		System.out.println();
+		for (int i = 0; i < parts.length; i++)
+			if(Integer.parseInt(parts[i])<=1000)
+				decimal += Integer.parseInt(parts[i]);
+		return decimal;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -1,14 +1,88 @@
 package it.uniba.tdd.tests;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
+
 public class StringCalculatorTest {
+	StringCalculator stringCalculator;
+
+	@Before
+	public void newStringCalculator() {
+		stringCalculator = new StringCalculator();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyString() throws StringCalculatorException {
+		Assert.assertEquals(0, stringCalculator.add(""));
+	}
+
+	@Test
+	public void testOneStringNumberOne() throws StringCalculatorException {
+		Assert.assertEquals(1, stringCalculator.add("1"));
+	}
+
+	@Test
+	public void testOneStringNumberTwo() throws StringCalculatorException {
+		Assert.assertEquals(2, stringCalculator.add("2"));
+	}
+
+	@Test
+	public void testOneStringNumber2000() throws StringCalculatorException {
+		Assert.assertEquals(2000, stringCalculator.add("2000"));
+	}
+
+	@Test
+	public void testOnePlusOne() throws StringCalculatorException {
+		Assert.assertEquals(2, stringCalculator.add("1,1"));
+	}
+
+	@Test
+	public void testOnePlus999() throws StringCalculatorException {
+		Assert.assertEquals(1000, stringCalculator.add("1,999"));
+	}
+	
+	@Test
+	public void testThreeNumbers() throws StringCalculatorException {
+		Assert.assertEquals(2000, stringCalculator.add("1,999\n1000"));
+	}
+
+	@Test
+	public void testOnePlus999WithNewLine() throws StringCalculatorException {
+		Assert.assertEquals(1000, stringCalculator.add("1\n999"));
+	}
+
+	@Test(expected = StringCalculatorException.class)
+	public void testInvalidStringWithCommaAndNewLine() throws StringCalculatorException {
+		stringCalculator.add("1,\n999");
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testInvalidStringWithNewLineAndComma() throws StringCalculatorException {
+		stringCalculator.add("1\n,9");
+	}
+	
+	@Test
+	public void testTheCustomDelimiter() throws StringCalculatorException {
+		Assert.assertEquals(3, stringCalculator.addWithCustomDelimiter("//;\n1;2"));
+	}
+	
+	@Test
+	public void testIgnoreNumbersOver1000() throws StringCalculatorException {
+		Assert.assertEquals(2, stringCalculator.addWithCustomDelimiter("//;\n1200;2"));
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void testInvalidNegativeNumber() throws StringCalculatorException {
+		stringCalculator.add("1,-1");
+	}
+	
+	@Test
+	public void testTheCustomDelimiterLengthOf3() throws StringCalculatorException {
+		Assert.assertEquals(6, stringCalculator.addWithCustomDelimiter("//[***]\n1***2***3"));
 	}
 
 }
